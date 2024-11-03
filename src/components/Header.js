cb9d6afd9a548c2b90490e966223dea6dca6b470
ajea <smithajea@rocketmail.com>
2024-11-02 23:51:18 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Header() {
+	return (
+		<header className="p-5 bg-slate-800 h-16 flex justify-between">
+			<h2>Logo</h2>
+			<nav>
+				<ul className="flex space-x-4">
+					<li>
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
