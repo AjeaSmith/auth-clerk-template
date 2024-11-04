9853243251a01491d4d3602736c00ef4e6094a74
ajea <smithajea@rocketmail.com>
2024-11-04 15:13:52 -0500

    remove unused imports from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Header() {
 	return (
